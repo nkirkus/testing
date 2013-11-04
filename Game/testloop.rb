d088bce8081b0537052b30abd887989f2a5c16b0
nkirkus <noah.kirkus@pharmmd.com>
2013-11-04 05:07:34 -0600

    Test push

---
+++ Game/testloop.rb
@@ -0,0 +1,4 @@
+i = -99
+puts "Before : " + i.to_s
+(1..10).each{|i| print 1; puts " haha"}
+puts "After : " + i.to_s
---
+++ rename.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+# Rename a file from test1.txt to test2.txt
+File.rename( "test2.txt", "test3.txt" )
+puts "test2.txt renamed to test3.txt"
